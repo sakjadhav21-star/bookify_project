c70600d8b36961aa3561142f6e6f8de34eef2d13
Sakshi <sak.jadhav.21@gmail.com>
2026-04-01 20:40:50 +0530

    first commit

---
+++ test_db.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+print("Connecting to database...")
+
+try:
+    conn = psycopg2.connect(
+        host="localhost",
+        database="bookify",
+        user="postgres",
+        password="sakshi",
+        port="5432"
+    )
+
+    print("Connected!")
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM train")
+    rows = cur.fetchall()
+
+    print("Data from Train table:")
+    for row in rows:
+        print(row)
+
+    conn.close()
+    print("Connection closed.")
+
+except Exception as e:
+    print("Error:", e)
