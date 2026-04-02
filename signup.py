b8a798415bca82ac4aa15eab83b2e136914f108e
Sakshii <sak.jadhav.21@gmail.com>
2026-04-02 09:34:57 +0530

    Update signup.py

--- signup.py
+++ signup.py
@@ -6,12 +6,12 @@ cur = conn.cursor()
 print("---- SIGN UP ----")
 name = input("Enter name: ")
 email = input("Enter email: ")
-password = input("Password: ),
+password = input("Password: "),
 age = input("Enter age: ")
 
 cur.execute(
-    "INSERT INTO user_table (name, email, age) VALUES (%s, %s, %s)",
-    (name, email, age)
+    "INSERT INTO user_table (name, email, password, age) VALUES (%s, %s, %s,  %s)",
+    (name, email, password, age)
 )
 
 conn.commit()
